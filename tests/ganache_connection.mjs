9729863fbcf3b1dc2a977f38381ca68a5f254832
Robert_Miller <robert.a.miller.410@gmail.com>
2025-02-15 21:45:52 -0600

    Event listener functions, they collect the necesarry front end data for the following: Image registration, voting, vote finalization

--- tests/ganache_connection.mjs
+++ tests/ganache_connection.mjs
@@ -1,10 +1,15 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: "../.env" });
 import { Web3, WebSocketProvider } from 'web3';
-
+import daiid from './DAIID.json' assert { type: 'json' };
+console.log(daiid)
 
 // Web Socket url
 const ganache_ws = process.env.GANACHE_WS;
+
+// address of the contract
+const address = process.env.DAIID_ADDRESS
+const abi = daiid['abi'];
 
 // Create a new WebsocketProvider
 const provider = new WebSocketProvider(
@@ -18,16 +23,62 @@ const provider = new WebSocketProvider(
 );
 const web3 = new Web3(provider);
 
-console.log("Listening for events...");
+function process_image_registration(data) {
+	console.log(data);
+	return data;
+}
 
-// Subscribe to all events
-web3.eth.subscribe("logs", {}, (error, event) => {
-    if (!error) {
-        console.log("New Event:", event);
-    } else {
-        console.error("Error:", error);
-    }
-});
+function process_vote_submission(data) {
+	console.log(data);
+	return data;
+}
 
-// Keep the script running
-process.stdin.resume();
+function process_vote_finalization(data) {
+	console.log(data);
+	return data;
+}
+
+async function subscribe_image_registration() {
+	// create a new contract object, providing the ABI and address
+	const contract = new web3.eth.Contract(abi, address);
+
+	// subscribe to the smart contract event (see DAIID.sol file)
+	const subscription = contract.events.ImageRegistered();
+
+	// new value every time the event is emitted
+	subscription.on('data', (event) => {
+		const data = {
+			address: event.address,
+			imageHash: event.returnValues[0],
+			ipfsCID: event.returnValues[1],
+		}
+		process_image_registration(data);
+	});
+}
+
+async function subscribe_vote_submission() {
+	const contract = new web3.eth.Contract(abi, address);
+	const subscription = contract.events.Voted();
+	subscription.on('data', (event) => {
+		const data = {
+			node: event.returnValues[0],
+			imageHash: event.returnValues[1],
+			score: event.returnValues[2],
+			weight: event.returnValues[3],
+		}
+		process_vote_submission(data);
+	});
+}
+
+async function subscribe_vote_finalization() {
+	const contract = new web3.eth.Contract(abi, address);
+	const subscription = contract.events.VoteFinalized();
+	subscription.on('data', (event) => {
+		const data = {
+			address: event.address,
+			imageHash: event.returnValues[0],
+			ipfsCID: event.returnValues[1],
+		}
+		process_vote_finalization(data);
+	});
+}
